61223478d4fb13a8fd5a60f12e99d5f64dc22e93
HarrietHarchy <aumaodima@gmail.com>
2017-03-09 09:50:49 +0300

    values of a car

---
+++ Car_class.py
@@ -0,0 +1,184 @@
+class Car(object):
+    def __init__(self, name, type,model):
+        self.name = name
+        self.type = type
+        self.model = model
+
+    
+    def name(self):
+        return self.__General
+
+    
+    def type(self):
+      return self.__Honda
+      
+    def model(self):
+      return self.__GM
+      
+      @property
+
+      def toyota(self):
+        return self.__toyota
+
+      @toyota.setter
+
+      def toyota(self, value):
+        toyota = str(value)
+  
+        self.__toyota = corolla(self._toyota)
+        
+        
+    def test_car_doors(self, opel,omega_3):
+        self.opel = opel
+        self.omega_3 = omega_3
+        
+    def test_car_doors(self, porshe,Turbo):
+        self.porshe = porshe
+        self.Turbo = Turbo
+
+    def __init__(self, name, type,model):
+        self.name = name
+        self.type = type
+        self.model = model
+
+    
+    def name(self):
+        return self.__General
+
+    
+    def type(self):
+      return self.__Honda
+      
+    def model(self):
+      return self.__GM
+      
+      
+      @property
+
+      def toyota(self):
+        return self.__toyota
+
+      @toyota.setter
+
+      def toyota(self, value):
+        toyota = str(value)
+  
+        self.__toyota = corolla(self._toyota)
+        
+        
+    def test_car_doors(self, opel,omega_3):
+        self.opel = opel
+        self.omega_3 = omega_3
+        
+    def test_car_doors(self, porshe,Turbo):
+        self.porshe = porshe
+        self.Turbo = Turbo
+        
+        @property
+
+        def __opel_num_of_doors(self):
+            return self.__opel.num_of_doors
+
+    @opel.num_of_doors.setter
+    def opel_num_of_doors(self, value):
+        opel.num_of_doors = str(value)
+        opel.num_of_doors = int(value)
+        
+        @property
+
+        def porshe_num_of_doors(self):
+            return self.__porshe.num_of_doors
+      
+    @opel.num_of_doors.setter
+
+    def porshe_num_of_doors(self, value):
+        porshe_num_of_doors(self, value)
+        porshe.num_of_doors = str(value)
+        porshe.num_of_doors = int(value)
+        
+    def test_car_wheels(self, MAN, Koenigsegg):
+        self.MAN=MAN
+        self.koenigsegg = koenigsegg
+        
+        @property
+
+        def MAN(self, Truck,trailer):
+            self_Truck = Truck
+            self_trailer=trailer
+
+    @MAN.setter
+    def MAN(self, value):
+        MAN.Truck = str(value)
+        MAN.trailer =str(value)
+        
+        @property
+
+        def speed(self):
+            return self.__speed
+
+    @speed.setter
+
+    def speed(self, value):
+        s = int(value)
+        s = max(0, s)
+        self.__speed = min(self.maxspeed, s)
+
+      
+    def doors(self, value):
+        doors = int(value)
+  
+        self.__doors = (self._toyota)
+
+        def opel.num_of_doors(self):
+            return self.__opel.num_of_doors
+
+    @opel.num_of_doors.setter
+    def opel.num_of_doors(self, value)
+        opel.num_of_doors = str(value)
+        opel.num_of_doors = int(value)
+        
+        @property
+
+        def porshe.num_of_doors(self):
+        return self.__porshe.num_of_doors
+      
+    @opel.num_of_doors.setter
+    def porshe.num_of_doors(self, value)
+        porshe.num_of_doors = str(value)
+        porshe.num_of_doors = int(value)
+        
+    def test_car_wheels(self, MAN, Koenigsegg)
+        self.MAN=MAN
+        self.koenigsegg = koenigsegg
+        
+        @property
+
+        def MAN(self, Truck,trailer):
+        self.Truck = Truck
+        self.trailer=trailer
+
+    @MAN.setter
+    def MAN(self, value)
+        MAN.Truck = str(value)
+        MAN.trailer =str(value)
+        
+        @property
+
+        def speed(self):
+        return self.__speed
+
+    @speed.setter
+    def speed(self, value):
+        s = int(value)
+        s = max(0, s)
+        self.__speed = min(self.maxspeed, s)
+        
+car = Car('Mercedez',100)
+car.speed = 100
+print 'My {name} is going {speed} mph!'.format(name=car.name, speed=car.speed)
+
+car.speed = 6000
+print 'My {name} is going {speed} mph!'.format(name=car.name, speed=car.speed)
+
+
+       
